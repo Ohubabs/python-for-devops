7ac4e72ee055e649cba2dde1d4853a3495d62303
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-01-17 12:15:19 -0800

    Update babs-python-install.sh

--- babs-python-install.sh
+++ babs-python-install.sh
@@ -3,6 +3,7 @@ sudo adduser python
 sudo echo "python ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/python
 sudo su - python
 sudo hostnamectl set-hostname python
+sudo timedatectl set-timezone America/Los_Angeles
 sudo chown python:python -R /etc/ssh/sshd_config
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
